7ce569fe5570e619528fd8a952131e5e144fc253
Anshu Kumar <anshu.ac.dv@gmail.com>
2023-04-05 15:03:20 +0530

    Initial Commit

---
+++ Includes/Header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <table>
+            <tr>
+                <td><a href="">Home</a></td>
+                <td><a href="">About</a></td>
+                <td><a href=""></a></td>
+            </tr>
+        </table>
+    </header>
+</body>
+</html>
